a3709575e692a2834ace25aa89d2d42d5a25299a
Shlomi Fish <shlomif@shlomifish.org>
2013-05-21 21:10:05 +0300

    Update tag release.

--- Test-WWW-Mechanize-Mojo/scripts/tag-release.pl
+++ Test-WWW-Mechanize-Mojo/scripts/tag-release.pl
@@ -7,7 +7,7 @@ use IO::All;
 
 my ($version) =
     (map { m{\$VERSION *= *'([^']+)'} ? ($1) : () }
-    io->file("lib/Test/WWW/Mechanize/Mojo.pm")->getlines()
+    io->file('lib/Test/WWW/Mechanize/Mojo.pm')->getlines()
     )
     ;
 
@@ -16,14 +16,12 @@ if (!defined ($version))
     die "Version is undefined!";
 }
 
-my $mini_repos_base = 'https://svn.berlios.de/svnroot/repos/web-cpan/Test-WWW-Mechanize-Mojo';
-
 my @cmd = (
-    "svn", "copy", "-m",
+    "hg", "tag", "-m",
     "Tagging the Test-WWW-Mechanize-Mojo release as $version",
-    "$mini_repos_base/trunk",
-    "$mini_repos_base/tags/releases/$version",
+    "releases/$version",
 );
 
 print join(" ", map { /\s/ ? qq{"$_"} : $_ } @cmd), "\n";
 exec(@cmd);
+
